a1091be7ee7ae0fb386b556e05055d881231f147
anurag007m <anurag007dk@gmail.com>
2023-09-01 18:00:47 +0530

    7

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
   // Sending This is the home page! in the page
   res
     .status(200)
-    .send(`This is the home and running on ${process.env.PORT}`);
+    .send(`This is the home and running on port ${process.env.PORT}`);
 });
 
 // Listening to the port 
